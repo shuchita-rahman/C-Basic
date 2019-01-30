eed6b1c3cca1236200566ca0320abf2ff2240d54
Shuchita <s.r.shuchi@gmail.com>
2019-01-30 20:52:57 +0600

    Add files via upload

---
+++ printingSentence.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    // printing a single line
+    printf("Hi I am new in C programming language");
+
+    //For to or more sentence you need to you need to give \n for new line
+
+    printf("\nHi I am new in C programming language.\nBut I think I will like it.");
+
+    return 0;
+}
