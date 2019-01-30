78dbe2ae989ef348ec35ca92f5d75e13c86cfcf4
Shuchita <s.r.shuchi@gmail.com>
2019-01-30 21:32:10 +0600

    Add files via upload

---
+++ forLoop.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+   //for loop variable increasing
+   printf("Ascending Order : ");
+   for(int i = 1; i <= 10; i++)
+   {
+       printf("\n%d", i);
+   }
+
+  //for loop variable increasing
+   printf("\nDescending Order : ");
+   for(int i = 10; i >= 1; i--)
+    {
+       printf("\n%d", i);
+   }
+
+
+    return 0;
+}
